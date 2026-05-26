72d4603d582f41319e2653b01c5aa2d667d79be1
miteneva.sofya.99 <miteneva.sofya.99@mail.ru>
2026-05-26 23:17:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ const charCount = document.getElementById('charCount');
 const wordCount = document.getElementById('wordCount');
 const sentenceCount = document.getElementById('sentenceCount');
 const longestWord = document.getElementById('longestWord');
+const timeResult = document.getElementById('timeResult');
 
 
 function analyzeText() {
+
+  // начало измерения времени
+  const start = performance.now();
+
   const text = textInput.value.trim();
 
   
@@ -23,18 +28,34 @@ function analyzeText() {
   sentenceCount.textContent = text.length === 0 ? 0 : sentences.length || 1;
 
   
-  const longest = words.reduce((max, word) => word.length > max.length ? word : max, '');
+  const longest = words.reduce(
+    (max, word) => word.length > max.length ? word : max,
+    ''
+  );
+
   longestWord.textContent = longest || '-';
+
+  // конец измерения времени
+  const end = performance.now();
+
+  // вычисление времени обработки
+  const processingTime = end - start;
+
+  // вывод времени на страницу
+  timeResult.textContent = processingTime.toFixed(3);
 }
 
 
 function clearText() {
   textInput.value = '';
+
   charCount.textContent = '0';
   wordCount.textContent = '0';
   sentenceCount.textContent = '0';
   longestWord.textContent = '-';
+  timeResult.textContent = '0';
 }
+
 
 analyzeBtn.addEventListener('click', analyzeText);
 clearBtn.addEventListener('click', clearText);
